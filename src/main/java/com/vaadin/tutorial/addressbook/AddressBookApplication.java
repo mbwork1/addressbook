009a183697ae579adbcbbc754fd00824d72a9601
TDW <mbwork1@yahoo.com>
2024-11-04 02:33:53 -0500

    updat appjava

--- src/main/java/com/vaadin/tutorial/addressbook/AddressBookApplication.java
+++ src/main/java/com/vaadin/tutorial/addressbook/AddressBookApplication.java
@@ -1,15 +1,32 @@
 package com.vaadin.tutorial.addressbook;
 
+// import org.springframework.boot.SpringApplication;
+// import org.springframework.boot.autoconfigure.SpringBootApplication;
+// import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+// @SpringBootApplication
+// public class AddressBookApplication extends SpringBootServletInitializer {
+//     public static void main(String[] args) {
+//         SpringApplication.run(AddressBookApplication.class, args);
+//     }
+// }
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+// import org.springframework.boot.web.support.SpringBootServletInitializer;  // OBSOLETE
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class AddressBookApplication extends SpringBootServletInitializer {
-    public static void main(String[] args) {
-        SpringApplication.run(AddressBookApplication.class, args);
+public class SpringBootWebApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(SpringBootWebApplication.class);
+    }
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(SpringBootWebApplication.class, args);
     }
 }
-
-
 
